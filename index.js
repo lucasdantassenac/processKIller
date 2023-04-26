d002312d61c0a676bb52248851cf18154a2b770b
Laboratório Informática <42labinfo@senacrjedu.local>
2023-04-26 19:58:00 -0300

    checking and killing process on existence

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const ps = require('ps-node');
 const event = require('events')
 let processCommand = "WindowsCalculator"
+let status = {
+    acive: false
 
+}
 // event.on("finishProcess", () => {
 //     ps.kill('Calculadora')
 //     });
 
-let checker = () => {
+let checkerF = () => {
     ps.lookup({ command: processCommand }, function(err, resultList ) {
         if (err) {
             throw new Error( err );
@@ -16,14 +19,17 @@ let checker = () => {
     
         if( process ){
             
-            console.log( 'PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments );
+            console.log( process)
             ps.kill(process.pid)
+            console.log('executou')
+            clearInterval(checker)
         }
-        else {
-            console.log( 'No such process found!' );
-        }
+
+        // else {
+        //     console.log( 'No such process found!' );
+        // }
     });
     
 }
 
-checker()
+let checker = setInterval(checkerF, 5000)
